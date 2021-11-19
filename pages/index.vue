ce824f774713cf13fdb3d03d2297c1287d1aa536
Dimas Raka Septiawan <dimasrakas@gmail.com>
2021-11-19 16:18:59 +0700

    add user agent detector

--- pages/index.vue
+++ pages/index.vue
@@ -20,7 +20,21 @@
 </template>
 
 <script>
-export default {}
+export default {
+  beforeCreate() {
+    console.log(this.$device.isMobileOrTablet)
+
+    if (this.$device.isMobileOrTablet) {
+      // eslint-disable-next-line nuxt/no-globals-in-created
+      window.location.href =
+        'AsiaCommerce://app.asiacommerce.net/product/product/163027'
+    } else {
+      // eslint-disable-next-line nuxt/no-globals-in-created
+      window.location.href =
+        'https://app.asiacommerce.net/product/product/163027'
+    }
+  },
+}
 </script>
 
 <style lang="scss" scoped></style>
