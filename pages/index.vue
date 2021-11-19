7470ad0980c31e7320a0143574b0a900576c1f81
Dimas Raka Septiawan <dimasrakas@gmail.com>
2021-11-19 15:59:45 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="p-4 mx-auto max-w-6xl">
+    <button href="https://app.asiacommerce.net/product/product/163027">
+      Web
+    </button>
+    <button href="AsiaCommerce://app.asiacommerce.net/product/product/163027">
+      iOS
+    </button>
+    <button href="https://app.asiacommerce.net/product/product/163027">
+      Android
+    </button>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
