0a57c07b17cbcae76545e6b5d1d58a334edffebd
Dimas Raka Septiawan <dimasrakas@gmail.com>
2021-11-19 16:21:53 +0700

    handler

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,12 @@
 <template>
   <div class="p-4 mx-auto max-w-6xl">
-    <button
-      onclick="location.href='https://app.asiacommerce.net/product/product/163027'"
-      href=""
-    >
-      Web
-    </button>
-    <button
-      onclick="location.href='AsiaCommerce://app.asiacommerce.net/product/product/163027'"
-    >
-      iOS
-    </button>
-    <button
-      onclick="location.href='https://app.asiacommerce.net/product/product/163027'"
-    >
-      Android
-    </button>
+    isMobile : {{ $device.isMobileOrTablet }}
   </div>
 </template>
 
 <script>
 export default {
   beforeCreate() {
-    console.log(this.$device.isMobileOrTablet)
-
     if (this.$device.isMobileOrTablet) {
       // eslint-disable-next-line nuxt/no-globals-in-created
       window.location.href =
